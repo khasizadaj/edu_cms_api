6610f37ad78cf8f78b67be3a0872c02238f13297
khasizadaj <javid.khasizada@gmail.com>
2024-01-14 04:29:32 +0100

    Move allowed hosts and debug value to .env file

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,25 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s-egoy@6qmg&y#7%!g0jy%tmxhb53tjuk_$(63u*f*h)*j953@"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+DEBUG = bool(int(os.getenv("DEBUG", 1)))
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -121,3 +114,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+if DEBUG is False:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+else:
+    SECRET_KEY = os.getenv("TEST_SECRET_KEY")
